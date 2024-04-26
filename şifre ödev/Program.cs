eead4b1fdf84256ef731dc2963fad32b6d53f898
Ahmet018 <112577154+Ahmet018@users.noreply.github.com>
2024-04-26 21:46:49 +0300

    Proje dosyası ekle.

---
+++ şifre ödev/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace şifre_ödev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //kullanıcı adı ve sifre oluşturdum
+            string ad = "admin";
+            string sifre = "12345";
+
+            //kullanıcı adı ve şifre istedim
+            Console.WriteLine("kullanıcı adı giriniz:");
+            string kullanici_adi = Console.ReadLine();
+
+            Console.WriteLine("şifre giriniz");
+            string passwd = Console.ReadLine();
+
+
+            //kullanıcı adı ve şifrenin başta oluşturduğum kullanıcı adı ve şifreye denk olmasını sağladım
+            if (kullanici_adi == ad && passwd == sifre)
+            {
+                Console.WriteLine("kullanıcı girişi başarılı");
+            }
+            //boş değer girilip girilmeğini kontrol ettim
+            else if (kullanici_adi == "" && passwd == "")
+            {
+                Console.WriteLine("kullanıcı adı ve sifre alanı boş bırakılamaz");
+            }
+            else
+            {
+                Console.WriteLine("hatalı giriş");
+            }
+            Console.ReadLine();
+        }
+    }
+}
